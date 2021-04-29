93cbc2ae48a4162b10b31ac7aa32ad5e2c5c6fb5
luismartinezs <martinez.s.luis@gmail.com>
2021-04-29 12:36:54 +0200

    Vuetify form validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,15 +3,23 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
-          <v-text-field label="email" type="email"></v-text-field>
+        <v-form ref="signUpForm" v-model="formValidity">
+          <v-text-field label="email" type="email" v-model="email" :rules="emailRules" required></v-text-field>
           <v-autocomplete label="Which browser do you use?" :items="browsers"></v-autocomplete>
           <v-file-input label="Attach profile picture"></v-file-input>
           <v-text-field label="Birthday" v-model="birthday" readonly></v-text-field>
           <v-date-picker v-model="birthday"></v-date-picker>
-          <v-checkbox label="Agree to terms and conditions"></v-checkbox>
+          <v-checkbox
+            label="Agree to terms and conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
           <!-- <v-switch label="Agree to terms and conditions"></v-switch> -->
-          <v-btn color="primary" type="submit">Submit</v-btn>
+          <v-btn color="primary" class="mr-4" type="submit" :disabled="!formValidity">Submit</v-btn>
+          <v-btn class="mr-4" @click="validateForm" color="success">Validate form</v-btn>
+          <v-btn color="warning" class="mr-4" @click="resetValidation">Reset validation</v-btn>
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -22,9 +30,39 @@
 export default {
   data() {
     return {
+      formValidity: false,
+      email: "",
+      emailRules: [
+        (value) => !!value || "Email is required",
+        (value) => value.indexOf("@") !== 0 || "Email should have a username",
+        (value) => value.includes("@") || "Email should include an @ symbol",
+        (value) =>
+          value.indexOf(".") - value.indexOf("@") > 1 ||
+          "Email should contain a valid domain",
+        (value) =>
+          value.indexOf(".") <= value.length - 3 ||
+          "Email should contain a valid domain extension",
+      ],
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        (value) =>
+          !!value ||
+          "You must agree to the terms and conditions to signup for an account",
+      ],
       birthday: "",
       browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
     };
+  },
+  methods: {
+    resetForm() {
+      this.$refs.signUpForm.reset();
+    },
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation();
+    },
+    validateForm() {
+        this.$refs.signUpForm.validate()
+    },
   },
 };
 </script>
